074621a141e52a27804fe1adb810812e7b02abfa
n <nerijus.gedrimas@gmail.com>
2026-05-22 18:10:34 +0300

    fixed #2 - remove menu.sh script

--- init.sh
+++ init.sh
@@ -5,21 +5,20 @@ PROJECT_ROOT="$HOME/projects/php-chat"
 
 tmux new-session -d -s $SESSION_NAME -n root
 
-tmux new-window -t $SESSION_NAME:1 -n scripts
-tmux new-window -t $SESSION_NAME:2 -n emacs
-tmux new-window -t $SESSION_NAME:3 -n php
-tmux new-window -t $SESSION_NAME:4 -n nginx
-tmux new-window -t $SESSION_NAME:5 -n react
-tmux new-window -t $SESSION_NAME:6 -n docker
+tmux new-window -t $SESSION_NAME:1 -n emacs -c $PROJECT_ROOT
+tmux new-window -t $SESSION_NAME:2 -n php -c $PROJECT_ROOT
+tmux new-window -t $SESSION_NAME:3 -n nginx -c $PROJECT_ROOT
+tmux new-window -t $SESSION_NAME:4 -n react -c $PROJECT_ROOT
+tmux new-window -t $SESSION_NAME:5 -n docker -c $PROJECT_ROOT
 
 sleep 0.2
 
 tmux send-keys -t $SESSION_NAME:0 "cd $PROJECT_ROOT" C-m
-tmux send-keys -t $SESSION_NAME:1 "bash $PROJECT_ROOT/scripts/menu.sh $PROJECT_ROOT" C-m
-tmux send-keys -t $SESSION_NAME:2 "cd $PROJECT_ROOT" C-m
-tmux send-keys -t $SESSION_NAME:3 "cd $PROJECT_ROOT" C-m
-tmux send-keys -t $SESSION_NAME:4 "cd $PROJECT_ROOT" C-m
-tmux send-keys -t $SESSION_NAME:5 "cd $PROJECT_ROOT" C-m
-tmux send-keys -t $SESSION_NAME:6 "cd $PROJECT_ROOT" C-m
 
-tmux attach -t $SESSION_NAME:1
+tmux send-keys -t $SESSION_NAME:1 "e" C-m
+tmux send-keys -t $SESSION_NAME:2 "docker compose exec php bash" C-m
+tmux send-keys -t $SESSION_NAME:3 "docker compose exec nginx bash" C-m
+tmux send-keys -t $SESSION_NAME:4 "docker compose exec react sh" C-m
+tmux send-keys -t $SESSION_NAME:5 "docker compose up" C-m
+
+tmux attach -t $SESSION_NAME:5
